01b1cdc21c71e2eb445de7c5a184b83b2ed0113d
jmalcaraz <j95@cuma.es>
2017-12-05 18:29:40 +0100

    Rest of files

---
+++ CefSharp.MinimalExample.WinForms/CustomKeyboardHandler.cs
@@ -0,0 +1,27 @@
+﻿// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System.Windows.Forms;
+using CefSharp;
+
+namespace CefSharp.MinimalExample
+{
+    class CustomKeyboardHandler : IKeyboardHandler
+    {
+        public bool OnKeyEvent(IWebBrowser browserControl, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey)
+        {
+            return false;
+        }
+
+        public bool OnPreKeyEvent(IWebBrowser browserControl, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
+        {
+            if (type == KeyType.RawKeyDown && windowsKeyCode == (int) Keys.F12)
+            {
+                browser.ShowDevTools();
+                return true;
+            }
+            return false;
+        }
+    }
+}
